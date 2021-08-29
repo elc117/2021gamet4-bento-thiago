0b6857f86d44fbbbf140425c1ac22ea125cf89dd
Bento Borges Schirmer <bento.schirmer@gmail.com>
2021-08-29 00:16:30 -0300

    Some practice with tiled

---
+++ game/core/assets/tmw_desert_spacing-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="tmw_desert_spacing-2" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing-2.png" width="265" height="199"/>
+</tileset>
