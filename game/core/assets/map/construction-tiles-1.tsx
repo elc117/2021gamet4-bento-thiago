1eec1a94b71a440cadd36f8862f47c28b2d4285a
Backendishappywithjavascript <wow@primasis.com>
2021-09-06 17:43:38 -0300

    Made a lot of changes in main map

---
+++ game/core/assets/map/construction-tiles-1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="construction-tiles-1" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="tiles/construction-tiles-1.png" width="640" height="640"/>
+ <tile id="19">
+  <objectgroup draworder="index" id="4">
+   <object id="5" x="0.425306" y="0.850611" width="30.8772" height="30.4519"/>
+  </objectgroup>
+ </tile>
+</tileset>
